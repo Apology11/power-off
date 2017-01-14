9d3a60510c44abf31b98733c3c8fc1f6e24f2748
Apology11 <apology11@gmail.com>
2017-01-14 18:56:48 +0100

    add reboot command

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var isLinux = require('is-linux'),
 	isWindows = require('is-windows'),
 	cp = require('child_process');
 
-module.exports = function (cb) {
+module.exports.shutdown = function (cb) {
 	var cmd = '';
 
 	if(isLinux() || isOsx()) {
@@ -18,4 +18,21 @@ module.exports = function (cb) {
 	cp.exec(cmd, function (err, stdout, stderr) {
 		cb(err, stout, stderr);
 	});
+};
+
+module.exports.reboot = function (cb) {
+	var cmd = '';
+	
+	if(isLinux() || isOsx()) {
+		cmd = 'sudo reboot';
+	} else if(isWindows()) {
+		cmd = 'shutdown -r -t 0';
+	} else {
+		throw new Error('Unknown OS!');
+	}
+
+	cp.exec(cmd, function (err, stdout, stderr) {
+		cb(err, stout, stderr);
+	});
+
 };
